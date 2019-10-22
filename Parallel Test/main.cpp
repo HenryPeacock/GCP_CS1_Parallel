c6724397b0bded801d5ecc47d74d23dab92e7fa0
Henry Peacock <henster23@hotmail.co.uk>
2019-10-22 17:36:41 +0100

    Added basic thread and mutex functionality

--- Parallel Test/main.cpp
+++ Parallel Test/main.cpp
@@ -1,3 +1,63 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+
+std::mutex mtx;
+
+void tFunc1(int _amt)
+{
+	for (int i = 0; i < _amt; i++)
+	{
+		mtx.lock();
+		std::cout << "Thread 1 ticking: " << i << std::endl;
+		mtx.unlock();
+	}
+}
+
+void tFunc2(int _amt)
+{
+	for (int i = 0; i < _amt; i++)
+	{
+		mtx.lock();
+		std::cout << "Thread 2 ticking: " << i << std::endl;
+		mtx.unlock();
+	}
+}
+
+void tFunc3(int _amt)
+{
+	for (int i = 0; i < _amt; i++)
+	{
+		mtx.lock();
+		std::cout << "Thread 3 ticking: " << i << std::endl;
+		mtx.unlock();
+	}
+}
+
+void tFunc4(int _amt)
+{
+	for (int i = 0; i < _amt; i++)
+	{
+		mtx.lock();
+		std::cout << "Thread 4 ticking: " << i << std::endl;
+		mtx.unlock();
+	}
+}
+
+int main()
+{
+
+	std::thread thread1(tFunc1, 250000);
+	std::thread thread2(tFunc2, 250000);
+	std::thread thread3(tFunc3, 250000);
+	std::thread thread4(tFunc4, 250000);
+
+	thread1.join();
+	thread2.join();
+	thread3.join();
+	thread4.join();
+
+	std::cin.get();
+
+	return 0;
+}
